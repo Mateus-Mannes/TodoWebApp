85a269752fae3a572b3d90885b2317b44b5e85d8
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-01-06 08:19:51 -0300

    Creating generic repository

---
+++ src/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(object id);
+        Task InsertAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task DeleteAsync(object id);
+    }
+}
---
+++ src/Repositories/Repository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+
+namespace TodoApp.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly TodoAppDbContext _context;
+        private DbSet<T> table;
+        public Repository(TodoAppDbContext context)
+        {
+            _context = context;
+            table = _context.Set<T>();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await table.ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(object id)
+        {
+            return await table.FindAsync(id);
+        }
+        public async Task InsertAsync(T obj)
+        {
+            await table.AddAsync(obj);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(T obj)
+        {
+            table.Update(obj);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(object id)
+        {
+            T existing = await table.FindAsync(id);
+            table.Remove(existing);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
