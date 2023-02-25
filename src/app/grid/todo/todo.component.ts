7fa1dce117d7579a10fa936cacf5c3819a473acc
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-02-25 15:09:19 -0300

    adding todo actions

--- src/app/grid/todo/todo.component.ts
+++ src/app/grid/todo/todo.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Todo } from 'src/app/entities/todo';
 
 @Component({
   selector: 'app-todo',
@@ -8,9 +9,22 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class TodoComponent implements OnInit {
 
+  @Output() completeTodo : EventEmitter<Todo> = new EventEmitter<Todo>();
+  @Output() editTodo : EventEmitter<Todo> = new EventEmitter<Todo>();
+
+  @Input() todo : Todo;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  emitTodoComplete(){
+    this.completeTodo.emit(this.todo);
+  }
+
+  emitTodoEdit(){
+    this.editTodo.emit(this.todo);
   }
 
 }
