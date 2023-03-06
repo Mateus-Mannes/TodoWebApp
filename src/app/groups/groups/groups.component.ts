05758040b187219938bf9fec76882af0c593428d
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-03-06 08:02:11 -0300

    adding groups crud methods

--- src/app/groups/groups/groups.component.ts
+++ src/app/groups/groups/groups.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AlertService } from 'src/app/alert-service';
 import { Todo } from 'src/app/entities/todo';
 import { TodoGroup } from 'src/app/entities/todo-group';
 
@@ -9,7 +11,7 @@ import { TodoGroup } from 'src/app/entities/todo-group';
 })
 export class GroupsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly _httpClient: HttpClient,private readonly _alertService : AlertService) { }
 
   ngOnInit(): void {
   }
@@ -17,5 +19,40 @@ export class GroupsComponent implements OnInit {
   @Input() groups: TodoGroup[] = [{ id:0, name:'Todos', slug:'todos', todos:[new Todo()] },{ id:0, name:'g1', slug:'g1', todos:[new Todo()] }, { id:0, name:'g2', slug:'g2', todos:[new Todo(), new Todo()] }];
   todos : TodoGroup | undefined = this.groups.find(x => x.slug = 'todos');
   @Output() changeGroup : EventEmitter<TodoGroup>;
+  loading = false;
+
+  createGroup(groupName: string){
+    this.loading  = true
+    this._httpClient.post<TodoGroup>('todo-group', groupName).subscribe({
+      next: res => {
+        this.loading  = false;
+        this.emitChangeGroup(res);
+      },
+      error: value => {
+        this._alertService.alert('Error on adding list - '+value.message, 'danger');
+        this.loading  = false;
+      }
+    });
+  }
+
+  deleteGroup(group: TodoGroup){
+    this.removeList(group);
+    this._httpClient.delete<TodoGroup>(`todo-group/${group.id}`).subscribe({
+      next: res => {},
+      error: value => {
+        this._alertService.alert('Error on deleting list - '+value.message, 'danger');
+        this.groups.push(group);
+      }
+    });
+  }
+
+  removeList(group: TodoGroup){
+    let index = this.groups.indexOf(group);
+    this.groups.splice(index, 1);
+  }
+
+  emitChangeGroup(group: TodoGroup){
+    this.changeGroup.emit(group);
+  }
 
 }
