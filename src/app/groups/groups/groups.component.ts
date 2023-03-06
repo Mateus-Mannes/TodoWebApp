bdf36d7ab48e45333f509002f64ccf678ad48c74
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-03-06 07:32:21 -0300

    displaying todos groups

--- src/app/groups/groups/groups.component.ts
+++ src/app/groups/groups/groups.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/entities/todo';
+import { TodoGroup } from 'src/app/entities/todo-group';
 
 @Component({
   selector: 'app-groups',
@@ -11,5 +13,9 @@ export class GroupsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() groups: TodoGroup[] = [{ id:0, name:'Todos', slug:'todos', todos:[new Todo()] },{ id:0, name:'g1', slug:'g1', todos:[new Todo()] }, { id:0, name:'g2', slug:'g2', todos:[new Todo(), new Todo()] }];
+  todos : TodoGroup | undefined = this.groups.find(x => x.slug = 'todos');
+  @Output() changeGroup : EventEmitter<TodoGroup>;
 
 }
