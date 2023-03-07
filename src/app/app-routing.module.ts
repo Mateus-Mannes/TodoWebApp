5f7a0f2afd956953104321bb2903f9a742fcecd5
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-03-07 07:43:38 -0300

    removing application component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
-import { GridComponent } from './grid/grid/grid.component';
 import { MatIconModule } from '@angular/material/icon';
-import { TodoComponent } from './grid/todo/todo.component';
-import { InputComponent } from './input/input/input.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ApplicationComponent } from './application/application.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { GroupsComponent } from './groups/groups/groups.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: ApplicationComponent },
   {path: 'login', component: LoginComponent},
   {path: 'user', component: CreateUserComponent},
   {path: 'groups', component: GroupsComponent}
